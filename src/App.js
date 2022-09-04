5924e5c43046588864d97da18c74985743680817
haisamk18 <khurshidhaisam@gmail.com>
2022-09-04 17:14:45 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import { Main } from './Pages/main';
+import  Main  from './Pages/Main';
+import Logo from './images/logo.jpg';
+import New from './Pages/new';
 
 function App() {
+  const mystyle ={
+      position: "absolute",
+      width: "70px",
+      height: "70px",
+      left: "65px",
+      top: "15px",
+      
+
+      
+
+
+
+  };
   return (
     <div className="App">
-       Hello world
-       <Main/>
+      <div className='mainapp'>
+              <div className='sidebar'></div>
+
+           <New/>
+       
+     </div>
+
     </div>
   );
 }
